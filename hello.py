8e9107105f7c27af7a37df95c4e535216e81fb03
yqq-a <qiuchanye7@gmail.com>
2025-09-01 12:10:13 +0800

    Add Python Hello World program

---
+++ hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Hello World program in Python
+"""
+
+def main():
+    print("Hello, World! 🌍")
+    print("你好，世界！")
+    print("こんにちは世界！")
+    print("¡Hola Mundo!")
+
+if __name__ == "__main__":
+    main()
